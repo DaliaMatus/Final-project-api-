d67a5b0c700850c1e2b417c8e192dd0679b4c182
DaliaMatus <daliamatus@gmail.com>
2020-08-13 01:15:34 -0500

    I created util.js, added the path for authentication and installed jsonwebtoken

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export default{
-    MONGODB_URL:process.env.MONGODB_URL || 'mongodb://localhost/api'
+    MONGODB_URL:process.env.MONGODB_URL || 'mongodb://localhost/api',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
