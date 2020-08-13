6626cbfd7987330a4d60d132a566baefcac84971
DaliaMatus <daliamatus@gmail.com>
2020-08-13 00:44:41 -0500

    I created routes,models and installed dotenv and moongose to connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import data from './database';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+import data from './database';
+import config from './config';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl= config.MONGODB_URL;
+mongoose.connect(mongodbUrl,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useCreateIndex:true
+}).catch(error => console.log(error.reason));
 const app=express();
 
+app.use('/api/users', userRoute);
 app.get('/api/products/:id',(req, res)=>{ 
   const productId =req.params.id;
   const product = data.products.find(x=>x._id ===productId);
