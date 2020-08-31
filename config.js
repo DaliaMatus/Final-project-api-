4f14892fd45f7c661ef3b9236d8826ad22808049
DaliaMatus <daliamatus@gmail.com>
2020-08-31 16:47:16 -0500

    new update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default{
     PORT:process.env.PORT || 8000,
-    MONGODB_URI:process.env.MONGODB_URI || 'mongodb://localhost/api',
+    MONGODB_URL:process.env.MONGODB_URL || 'mongodb://localhost/api',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
