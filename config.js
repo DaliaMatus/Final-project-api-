9d611b2170b7f60bd5087dd35cfadc3ededaecad
DaliaMatus <daliamatus@gmail.com>
2020-09-02 18:34:42 -0500

    I changed the database path from local to MongoDB Atlas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default{
     PORT:process.env.PORT || 8000,
-    MONGODB_URL:process.env.MONGODB_URL || 'mongodb://localhost/api',
+    MONGODB_URL:process.env.MONGODB_URL || 'mongodb+srv://Dalia_Matus:Dalia_Matus@cluster0.le0jt.mongodb.net/<dbname>?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
